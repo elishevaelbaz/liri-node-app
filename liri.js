9cb049a61f95a0c2f443ef722f8d4ef867bc53fa
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-08 19:03:53 -0500

    added movie-this functionality

--- liri.js
+++ liri.js
@@ -3,6 +3,13 @@ var twitterInfo = require("./keys.js");
 
 var twitter = require("twitter");
 
+var Spotify = require('node-spotify-api');
+
+var request = require("request");
+
+ 
+
+
 
 
  // console.log(twitterInfo);
@@ -27,9 +34,50 @@ function displayTweets(){
 	});
 }
 
+function displaySongInfo(song){
+	var spotify = new Spotify({
+		id: "9791b8f8bdd2445294356216abd9f845",
+		secret: "a0de4ef3219f4d0c820c66c8dedb6b31"
+	});
+ 
+	spotify.search({ type: 'track', query: song }, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	 
+	console.log(data); 
+	console.log(data.tracks.items[0]); 
+	});
+}
 
-// GET https://api.twitter.com/1.1/search/tweets.json?q=@3lemonstar&count=20
-// console.log(client);
+
+function displayMovieInfo(movie){
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&plot=short&apikey=trilogy"
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    var infoArray = ["Title", "Year", "Country", "Language", "Plot", "Actors"];
+    for (var i = 0; i < infoArray.length; i++) {
+    	console.log(infoArray[i] + ": " + JSON.parse(body)[infoArray[i]]);
+    }
+
+   	console.log("imdb Rating: " + JSON.parse(body).Ratings[0].Value);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+   
+  }
+
+});
+
+}
+
+
 
 switch(process.argv[2]) {
 
@@ -37,12 +85,12 @@ switch(process.argv[2]) {
     displayTweets();
     break;
 
-  // case "withdraw":
-  //   withdraw(process.argv[3]);
-  //   break;
+  case "spotify-this-song":
+  	displaySongInfo(process.argv[3]);
+    break;
 
-  // case "deposit":
-  //   deposit(process.argv[3]);
+  case "movie-this":
+  		displayMovieInfo(process.argv[3]);
   //   break;
   
   // case "lotto":
@@ -52,3 +100,6 @@ switch(process.argv[2]) {
   // default:
   //   console.log("Please enter 'total' 'withdraw' 'deposit' or 'lotto'")
 }
+
+
+
