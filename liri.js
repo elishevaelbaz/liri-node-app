0e00c3ac57be98c2108f1adc8c77d4c4db58e17c
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-09 21:08:42 -0500

    using inquirer with comments. removed the file without inquirer

--- liri.js
+++ liri.js
@@ -9,18 +9,18 @@ var request = require("request");
 
 var fs = require("fs");
 
+var inquirer = require("inquirer");
 
 
- // console.log(twitterInfo);
 
-var client = new Twitter({
+function displayTweets(){
+
+	var client = new Twitter({
   consumer_key: twitterInfo.consumer_key,
   consumer_secret: twitterInfo.consumer_secret,
   access_token_key: twitterInfo.access_token_key,
   access_token_secret: twitterInfo.access_token_secret
 });
-
-function displayTweets(){
 
 	var params = {screen_name: '3lemonstar'};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -28,16 +28,19 @@ function displayTweets(){
 	    console.log(error);
 	  }
 
-	  console.log(tweets);
-
-	  tweets.forEach(function(tweet){
+	  // forEach tweet in the response ('tweets'), display the text and date created
+	  tweets.forEach(function(tweet, num){
+	  	  	
+	  	console.log("\ntweet #" + (num + 1));
 	  	console.log(tweet.text);
-	  console.log("Date created: " + tweet.created_at);
+	  	console.log("Date created: " + tweet.created_at);
 	  })
 
+// or can use for loop
 	  // for (var i = 0; i < tweets.length; i++) {
+	  // 	console.log("\ntweet #" + (i + 1));
 	  // 	console.log(tweets[i].text);
-	  // 		console.log("Date created: " + tweets[i].created_at);
+	  // 	console.log("Date created: " + tweets[i].created_at);
 	  // }
 	  
 	});
@@ -49,38 +52,17 @@ function displaySongInfo(song){
 		secret: "a0de4ef3219f4d0c820c66c8dedb6b31"
 	});
 
-	if(process.argv[3] == undefined){
-		song = "Ace of Base"
-
-	}
  
 	spotify.search({ type: 'track', query: song }, function(err, data) {
 	  if (err) {
 	    return console.log('Error occurred: ' + err);
 	  }
 
-	  // if (!data){
-	  // 	spotify.search({ type: 'track', query: song }, function(err, data) {
-	  // 	if (err) {
-	  //   	return console.log('Error occurred: ' + err);
-	  // 	}
-
-
-
-
-	  // })
-	  // }
-	 
-	// console.log("data" + data[0]); 
-
-	console.log("You made it here")
-
-	console.log(data.tracks.items[0].name);
-	console.log(data.tracks.items[0].artists[0].name);  
-	console.log(data.tracks.items[0].preview_url);
-	console.log(data.tracks.items[0].album.name);  
-
-
+	  console.log(data.tracks.items[0])
+		console.log("Song Name: " + data.tracks.items[0].name);
+		console.log("Artist(s): " + data.tracks.items[0].artists[0].name);  
+		console.log("Preview Link: " + data.tracks.items[0].preview_url);
+		console.log("Album: " + data.tracks.items[0].album.name);  
 	 
 	});
 }
@@ -96,8 +78,8 @@ function displayMovieInfo(movie){
 	  // If the request is successful (i.e. if the response status code is 200)
 	  if (!error && response.statusCode === 200) {
 
-	    // Parse the body of the site and recover just the imdbRating
-	    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+	    // Parse the body of the site and recover the title, year, imdbRating etc
+	    // created an array and looped through it instead of having so many console.logs
 	    var infoArray = ["Title", "Year", "Country", "Language", "Plot", "Actors"];
 	    for (var i = 0; i < infoArray.length; i++) {
 	    	console.log(infoArray[i] + ": " + JSON.parse(body)[infoArray[i]]);
@@ -124,7 +106,7 @@ function readFromFile(){
 	  // We will then print the contents of data
 	  console.log(data);
 
-	  // Then split it by commas (to make it more readable)
+	  // Then split it by commas
 	  var dataArr = data.split(",");
 
 	  // We will then re-display the content as an array for later use.
@@ -132,6 +114,7 @@ function readFromFile(){
 
 	  console.log("node liri.js " + dataArr[0] + " " + dataArr[1]);
 
+	  //0th element of dataArr is one of the commands
 	  switch(dataArr[0]){
 
   		case "my-tweets":
@@ -150,31 +133,77 @@ function readFromFile(){
 	});
 }
 
+// prompt the user to choose a command
+inquirer.prompt([
 
-
-
-switch(process.argv[2]) {
-
-  case "my-tweets":
-    displayTweets();
-    break;
-
-  case "spotify-this-song":
-  	displaySongInfo(process.argv[3]);
-    break;
-
-  case "movie-this":
-  	displayMovieInfo(process.argv[3]);
-    break;
-
-  case "do-what-it-says":
-  	readFromFile();
-
-    break;
-  	
-  default:
-    console.log("Please enter 'my-tweets', 'spotify-this-song', 'movie-this, or 'do-what-it-says'")
+{
+	type: "list", 
+	message: "Choose a command",
+	choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
+	name: "command"
 }
+// then, based on their choice 
+]).then(answers => {
+  
+	switch(answers.command) {
+		// (if user chose 'my-tweets', run the corresponding function
+	  case "my-tweets":
+	    displayTweets();
+	    break;
+
+	  case "spotify-this-song":
+	  // show another prompt to get the song name
+	  	inquirer.prompt([
+
+				{
+					type: "input", 
+					message: "Enter the name of a song (If none is provided I will use 'The Sign' by Ace of Base)",
+					name: "song"
+				}
+				// then run the function using user input for the song 
+				]).then(answers => {
+
+					//if no song was provided, use 'The Sign' by Ace of Base
+					if (!answers.song ){
+						answers.song = "The Sign, Ace of Base";
+					}
+					
+					displaySongInfo(answers.song);
+					
+				});
+
+	    break;
+
+	  case "movie-this":
+	  // show another prompt to get the movie name
+	  	inquirer.prompt([
+
+				{
+					type: "input", 
+					message: "Enter the name of a movie (If none is provided I will use 'Mr. Nobody')\n",
+					name: "movie"
+				}
+				// then run the function using user input for the movie
+				]).then(answers => {
+					// if no movie was provided, use 'Mr. Nobody'
+					if (!answers.movie ){
+						answers.movie = "Mr. Nobody";
+					}
+
+					displayMovieInfo(answers.movie);
+				});
+
+	    break;
+
+	  case "do-what-it-says":
+	  	readFromFile();
+
+	    break;
+	}
+
+});
+
+
 
 
 
